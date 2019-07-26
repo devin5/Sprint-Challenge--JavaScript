a63706ee201c3a36e28e12ff2e7141ba74f5f862
Devin Dias <devindias.tech@gmail.com>
2019-07-26 12:59:15 -0500

    latest update friday july 26 .js challenge

--- challenges/functions.js
+++ challenges/functions.js
@@ -6,19 +6,37 @@
   * The last parameter accepts a callback 
   * In the body of the function return the callback with the two parameters that you created
 */
-
+console.log("Step 1: "); 
+function consume(x, y, cb){
+    return cb(x, y); 
+}
 
 /* Step 2: Create several functions to callback with consume();
   * Create a function named add that returns the sum of two numbers
   * Create a function named multiply that returns the product of two numbers 
   * Create a function named greeting that accepts a first and last name and returns "Hello first-name last-name, nice to meet you!"
 */
+function add(a, b){
+  return a + b; 
+}
+function multiply(a, b){
+  return a * b; 
+}
+function greeting(a, b){
+  return "Hello " + a + " " + b + ", nice to meet you!";
+}
 
 
 /* Step 3: Check your work by un-commenting the following calls to consume(): */
 // consume(2,2,add); // 4
+console.log(consume(2,2,add)); // 4
+console.log(""); 
 // consume(10,16,multiply); // 160
+console.log(consume(10, 16, multiply)); // 160
+console.log(""); 
 // consume("Mary","Poppins", greeting); // Hello Mary Poppins, nice to meet you!
+console.log(consume("Mary", "Poppins", greeting)); 
+console.log("");
 
 
 // ==== Closures ==== 
@@ -26,6 +44,7 @@
 // Explain in your own words why `nestedfunction()` can access the variable `internal`.
 
 // Explanation: 
+console.log("The function nestedfunction() can access the variable internal because they are in the same scope. The variable internal was created inside the function myFunction, and the function nestedFunction was also created inside the function myFunction. Because of this, they have visibility, and thus access, to one another. ")
 
 
 const external = "I'm outside the function";
